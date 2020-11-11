bed303da517a633d23f1b013f3276e92450cdda9
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-11-11 10:11:39 -0800

    added objectStore file

---
+++ public/indexedDB.js
@@ -0,0 +1,64 @@
+let db;
+
+// Create a new DB request
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = (e) => {
+	const db = e.target.result;
+
+	// Create Object Store and set key to autoincrement
+	db.createObjectStore('pending', { autoIncrement: true });
+};
+
+request.onsuccess = (e) => {
+	db = e.target.result;
+
+	if (navigator.onLine) {
+		checkDatabse();
+	}
+};
+
+request.onerror = (e) => {
+	console.log(`alert!: ${e.target.error}`);
+};
+
+function saveRecord(record) {
+	const transaction = db.transaction(['pending'], 'readwrite');
+	const store = transaction.objectStore('pending');
+
+	store.add(record);
+}
+
+function checkDatabase() {
+	// Open a transaction in the pending db
+	const transaction = db.transaction(['pending'], 'readwrite');
+	// Access pending object store
+	const store = transaction.objectStore('pending');
+	// Get all records from store and set to a variable
+	const getAll = store.getAll();
+
+	getAll.onsuccess = () => {
+		if (getAll.result.length > 0) {
+			fetch('/api/transaction/bulk', {
+				method: 'POST',
+				body: JSON.stringify(getAll.result),
+				headers: {
+					Accept: 'application/json, text/plain, */*',
+					'Content-Type': 'application/json',
+				},
+			})
+				.then((response) => response.json)
+				.then(() => {
+					// If successful. open a transaction in the pending db
+					const transaction = db.transaction(['pending'], 'readwrite');
+					// Access pending object store
+					const store = transaction.objectStore('pending');
+					// Clear all items in the store
+					store.clear();
+				});
+		}
+	};
+}
+
+// Listen for app coming back online
+window.addEventListener('online', checkDatabase);
--- public/sw_caches_pages.js
+++ public/sw_caches_pages.js
@@ -1,7 +1,16 @@
 const cacheName = 'v1';
+const dataCacheName = 'v1';
 
 // Cache Individual Files
-const cacheAssets = ['index.html', './styles.css', './index.js'];
+const cacheAssets = [
+	'/',
+	'index.html',
+	'./styles.css',
+	'./index.js',
+	'./manifest.webmanifest',
+	'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+	'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+];
 
 // Call Install Event
 self.addEventListener('install', (e) => {
@@ -29,7 +38,7 @@ self.addEventListener('activate', (e) => {
 		caches.key().then((cacheNames) => {
 			return Promise.all(
 				cacheNames.map((cache) => {
-					if (cache !== cacheName) {
+					if (cache !== cacheName && cache !== dataCacheName) {
 						console.log('Service Worker: Clearing Old Cache');
 						return caches.delete(cache);
 					}
@@ -37,14 +46,44 @@ self.addEventListener('activate', (e) => {
 			);
 		})
 	);
+
+	self.clients.claim();
 });
 
 // Call Fetch Event
 self.addEventListener('fetch', (e) => {
 	console.log('Service Worker: Fetching');
+
+	if (e.request.url.includes('/api')) {
+		console.log('Service Worker Fetch Data', e.request.url);
+		e.respondWith(
+			caches
+				.open(dataCacheName)
+				.then((cache) => {
+					return fetch(e.request)
+						.then((response) => {
+							if (response.status === 200) {
+								cache.put(e.request.url, response.clone());
+							}
+							return response;
+						})
+						.catch((err) => {
+							return cache.match(e.request);
+						});
+				})
+				.catch((err) => {
+					console.log(err);
+				})
+		);
+
+		return;
+	}
+
 	e.respondWith(
-		fetch(e.request).catch(() => {
-			caches.match(e.request);
+		caches.open(cacheName).then((cache) => {
+			return cache.match(e.request).then((response) => {
+				return response || fetch(e.request);
+			});
 		})
 	);
 });
